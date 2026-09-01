666770ea04629f1a642c12795c5764b8bc3b0774
Liverworks <56203382+Liverworks@users.noreply.github.com>
2026-09-01 15:25:28 +0200

    Update Genotype_short_reads.sh to add missing parts

--- Genotype_short_reads.sh
+++ Genotype_short_reads.sh
@@ -11,3 +11,16 @@ gatk GenomicsDBImport --java-options "-Xmx200G" \
     -V samples.list \
  --overwrite-existing-genomicsdb-workspace \
  --reader-threads 25  --genomicsdb-workspace-path Pollen_AL62NT1_2n
+ 
+ gatk --java-options "-Xmx85G -Xms78G -XX:ParallelGCThreads=8" GenotypeGVCFs -R NT1_220222.fasta -V gendb://Pollen_NT1NT8 --include-non-variant-sites -O Pollen_NT1NT8.vcf.gz
+
+vcf="Pollen_NT1NT8_2n.vcf.gz"
+
+
+# GATK best practices quality filter
+bcftools filter -Ou -S . -e 'FMT/DP<4' ${vcf} |
+  bcftools view -e "(N_MISSING==N_SAMPLES) || (QD<2.0 || FS>60.0 || SOR>3 || MQ<40.0 || MQRankSum<-12.0 || ReadPosRankSum<-8.0)" \
+  --threads 128 -Oz -Wtbi -o ${vcf%.vcf*}_qual.vcf.gz
+
+# Biallelic segregating sites
+bcftools view -m2 -M2 -v snps -e 'AC==0 || AC==AN' ${vcf%.vcf*}_qual.vcf.gz -Oz -Wtbi --threads 128 -o ${vcf%.vcf*}_bial.vcf.gz
